611468e51af3f4d17f8687a71402a01679896e07
vinicius.caldeira@stone.com.br <M@more123>
2021-03-03 20:05:34 -0300

    implementing test for chapter 1 - arrays and strings

--- 1-array-and-strings/IsUnique.cs
+++ 1-array-and-strings/IsUnique.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace _1_array_and_strings
 {
-    // 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+    // 1.1: Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
     // cannot use additional data structures? 
     public class IsUnique
     {
