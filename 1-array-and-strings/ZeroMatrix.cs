4ee5045cc86dc6d54bd5dc1e6dcbef9edfe95776
vinicius.caldeira@stone.com.br <M@more123>
2021-03-11 23:34:09 -0300

    finishing first chapter

--- 1-array-and-strings/ZeroMatrix.cs
+++ 1-array-and-strings/ZeroMatrix.cs
@@ -4,15 +4,47 @@ namespace _1_array_and_strings
     // column are set to 0. 
     public class ZeroMatrix
     {
-        // public static int[,] Execute(int[,] array)
-        // {
-        //     for (int i = 0; i < array.GetLength(0); i++)
-        //     {
-        //         for (int j = 0; j < array.GetLength(1); j++)
-        //         {
-        //             if (array[i, j] == 0)
-        //         }
-        //     }
-        // }
+        public static int[][] Execute(int[][] matrix)
+        {
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+
+            var rowZero = new bool[rows];
+            var colZero = new bool[cols];
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if (matrix[r][c] == 0)
+                        rowZero[r] = true;
+                    colZero[c] = true;
+                }
+            }
+
+            for (int r = 0; r < rows; r++)
+            {
+                if (rowZero[r])
+                {
+                    for (int c = 0; c < cols; c++)
+                    {
+                        matrix[r][c] = 0;
+                    }
+                }
+            }
+
+            for (int c = 0; c < cols; c++)
+            {
+                if (colZero[c])
+                {
+                    for (int r = 0; r < rows; r++)
+                    {
+                        matrix[r][c] = 0;
+                    }
+                }
+            }
+
+            return matrix;
+        }
     }
 }
--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 - [x] 1.1 - [Is Unique](1-array-and-strings/IsUnique.cs)
 - [x] 1.2 - [Check Permutation](1-array-and-strings/CheckPermutation.cs)
 - [x] 1.3 - [URLify](1-array-and-strings/URLify.cs)
-- [x] 1.4 - Palindrome Permutation
+- [x] 1.4 - [Palindrome Permutation](1-array-and-strings/PalindromePermutation.cs)
 - [x] 1.5 - [One Way](1-array-and-strings/OneWay.cs)
 - [x] 1.6 - [String Compression](1-array-and-strings/StringCompression.cs)
 - [x] 1.7 - [Rotate Matrix](1-array-and-strings/RotateMatrix.cs)
-- [ ] 1.8 - Zero Matrix
-- [ ] 1.9 - String Rotation
+- [x] 1.8 - [Zero Matrix](1-array-and-strings/ZeroMatrix.cs)
+- [x] 1.9 - [String Rotation]((1-array-and-strings/StringRotation.cs))
 
 ## Chapter 2 - Linked Lists
